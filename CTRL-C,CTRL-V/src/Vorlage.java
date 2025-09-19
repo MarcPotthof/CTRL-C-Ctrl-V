d5d2a4f7902b117ed628ba6ad653a50649db5042
Marc <PotthofMarc@gmail.com>
2025-09-19 22:31:56 +0200

    Chiffrevorschläge hinzugefügt

--- CTRL-C,CTRL-V/src/Vorlage.java
+++ CTRL-C,CTRL-V/src/Vorlage.java
@@ -3,6 +3,13 @@
 // Sie implementiert das Interface Cipher und muss daher die drei Methoden Verschluesseln, Entschluesseln und SchluesselInfo enthalten.
 // Derzeit sind die Methoden nicht implementiert und geben nur den Eingabetext bzw. einen Beispieltext zurück.
 // Die Klasse kann kopiert und umbenannt werden, um einen neuen Verschlüsselungsalgorithmus zu erstellen.
+
+// Hier einige Verschlüsselungsalgorithmen, die implementiert werden können:
+// - Caesar-Chiffre: https://de.wikipedia.org/wiki/Caesar-Verschl%C3%BCsselung
+// - Vigenère-Chiffre (etwas schwerer als die anderen, aber gut machbar): https://de.wikipedia.org/wiki/Vigen%C3%A8re-Chiffre
+// - einfaches Transpositionsverfahren: https://de.wikipedia.org/wiki/Spaltentransposition
+// - Enigma (zeitaufwändig, aber geschichtlich interessant): https://kryptografie.de/kryptografie/chiffre/enigma.htm
+// - was eigenes ausdenken, oder mehrere chiffren kombinieren :D
 public class Vorlage implements Cipher
 {
    // hier kann die echte Verschlüsselungslogik implementiert werden
